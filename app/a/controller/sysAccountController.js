7883d141507d435311006d9f49d2ee3a03e1eb0f
tongjichang <459132449@qq.com>
2018-08-24 23:40:11 +0800

    导航样式、延时加载

---
+++ app/a/controller/sysAccountController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//var app = angular.module("adminApp");
+
+angular.module("adminApp").controller('sysAccountController',['$scope','$http','$location','$window',function ($scope,$http,$location,$window){
+    alert("系统管理");
+}]);
