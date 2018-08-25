e62273022b29d9af41108a6e8cafd47e138daffb
tongjichang <459132449@qq.com>
2018-08-25 22:14:58 +0800

    新增商户

---
+++ app/a/controller/addShopController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jacktong on 2018/8/25.
+ */
+'use strict';
+
+var app = angular.module("adminApp");
+
+app.controller('addShopController',['$scope','$http','$location','$window',function ($scope,$http,$location,$window){
+    alert("add shop controller");
+
+}]);
