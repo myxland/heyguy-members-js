19bd0c9b0bcbae0fd77607735c58cbc6a3bd26a6
刘佳 <dbliujia@travelsky.com>
2018-08-30 16:52:31 +0800

    实现用户部分功能

--- app/b/controller/userController.js
+++ app/b/controller/userController.js
@@ -7,7 +7,6 @@ var app = angular.module("shopApp");
 
 app.controller('userController',['$scope','$http','$location','$window',function ($scope,$http,$location,$window){
 
-
     /**
      * 新增用户
      */
@@ -31,7 +30,7 @@ app.controller('userController',['$scope','$http','$location','$window',function
     $scope.getOnePageUser = function(){
         $http({
             method:"POST",
-            url:base_url+"/user/findAll",
+            url:base_url+"/user/customer/findAllUser",
             data:{
                 page:$scope.currentPage,
                 size:$scope.prePage
@@ -40,7 +39,7 @@ app.controller('userController',['$scope','$http','$location','$window',function
         }).success(function (data,status) {
             if(data.code=='0'){
                 console.log(data.data);
-                $scope.discountList = data.data.content;
+                $scope.userList = data.data.content;
                 $scope.totalItems = data.data.totalElements;
                 $scope.currentPage = data.data.number+1;
             }else{
@@ -53,5 +52,78 @@ app.controller('userController',['$scope','$http','$location','$window',function
     }
 
     $scope.getOnePageUser();
+
+    /**
+     * 删除账户
+     */
+    $scope.deleteUser = function(id){
+
+        var layer = layui.layer;
+        var dialog = layui.dialog;
+
+        dialog.confirm({
+            message:'您确定删除此账户？',
+            success:function(){
+                $http({
+                    method:"POST",
+                    url:base_url+"/user/admin/delete",
+                    data:{
+                        id:id
+                    },
+                    cache:false,
+                }).success(function (data,status) {
+                    if(data.code=='0'){
+                        layer.closeAll();
+                        $scope.getOnePageUser();
+                    }else{
+                        layer.alert(data.msg);
+                    }
+                }).error(function (response,status,header) {
+                        layer.alert('系统繁忙、稍后再试');
+                    });
+            },
+            cancel:function(){
+            }
+        });
+    }
+
+    $scope.changeStatus = function(id,status){
+        var msg;
+        if(status==1){
+            msg = "确定要解冻？";
+        }
+        if(status==0){
+            msg = "确定要冻结？";
+        }
+
+        var layer = layui.layer;
+        var dialog = layui.dialog;
+
+        dialog.confirm({
+            message:msg,
+            success:function(){
+                $http({
+                    method:"POST",
+                    url:base_url+"/user/customer/changeStatus",
+                    data:{
+                        id:id,
+                        status:status
+                    },
+                    cache:false,
+                }).success(function (data,status) {
+                    if(data.code=='0'){
+                        layer.closeAll();
+                        $scope.getOnePageUser();
+                    }else{
+                        layer.alert(data.msg);
+                    }
+                }).error(function (response,status,header) {
+                        layer.alert('系统繁忙、稍后再试');
+                    });
+            },
+            cancel:function(){
+            }
+        });
+    }
 
 }]);
--- app/filter.js
+++ app/filter.js
@@ -14,6 +14,18 @@ angular.module("adminApp").filter('user_status',function(){
         return status_name;
     }
 });
+angular.module("shopApp").filter('user_status_shop',function(){
+    return function(status){
+        var status_name = "";
+        if(status=="1"){
+            status_name = "正常";
+        }
+        if(status=="0"){
+            status_name = "冻结";
+        }
+        return status_name;
+    }
+});
 
 
 
