eb87863e5647d75d03bd3715be199f7e8c5e9ef4
刘佳 <dbliujia@travelsky.com>
2018-09-20 16:41:06 +0800

    主页页面开发

--- app/b/controller/mainController.js
+++ app/b/controller/mainController.js
@@ -17,7 +17,7 @@ angular.module("shopApp").controller('mainController',['$scope','$http','$locati
     $scope.getConsumeFeeList = function(){
         $http({
             method:"POST",
-            url:base_url+"/user/customer/findAllUser",
+            url:base_url+"/statistics/getConsumeFeeList",
             data:{
                 page:$scope.currentPage,
                 size:$scope.prePage,
@@ -27,9 +27,9 @@ angular.module("shopApp").controller('mainController',['$scope','$http','$locati
         }).success(function (data,status) {
             if(data.code=='0'){
                 console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
+                $scope.consumFeeList = data.data;
+                // $scope.totalItems = data.data.totalElements;
+                // $scope.currentPage = data.data.number+1;
             }else{
                 layui.layer.alert(data.msg);
             }
@@ -44,151 +44,151 @@ angular.module("shopApp").controller('mainController',['$scope','$http','$locati
     /**
      * 查询消费次数信息
      */
-    $scope.getConsumeCountList = function(){
-        $http({
-            method:"POST",
-            url:base_url+"/user/customer/findAllUser",
-            data:{
-                page:$scope.currentPage,
-                size:$scope.prePage,
-                shopId:admin_user.shopId
-            },
-            cache:false,
-        }).success(function (data,status) {
-            if(data.code=='0'){
-                console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
-            }else{
-                layui.layer.alert(data.msg);
-            }
-        })
-            .error(function (response,status,header) {
-                layui.layer.alert('系统繁忙、稍后再试');
-            });
-    }
-
-    $scope.getConsumeCountList();
-
-    /**
-     * 查询充值金额信息
-     */
-    $scope.getRechargeFeeList = function(){
-        $http({
-            method:"POST",
-            url:base_url+"/user/customer/findAllUser",
-            data:{
-                page:$scope.currentPage,
-                size:$scope.prePage,
-                shopId:admin_user.shopId
-            },
-            cache:false,
-        }).success(function (data,status) {
-            if(data.code=='0'){
-                console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
-            }else{
-                layui.layer.alert(data.msg);
-            }
-        })
-            .error(function (response,status,header) {
-                layui.layer.alert('系统繁忙、稍后再试');
-            });
-    }
-
-    $scope.getRechargeFeeList();
+    // $scope.getConsumeCountList = function(){
+    //     $http({
+    //         method:"POST",
+    //         url:base_url+"/user/customer/findAllUser",
+    //         data:{
+    //             page:$scope.currentPage,
+    //             size:$scope.prePage,
+    //             shopId:admin_user.shopId
+    //         },
+    //         cache:false,
+    //     }).success(function (data,status) {
+    //         if(data.code=='0'){
+    //             console.log(data.data);
+    //             $scope.userList = data.data.content;
+    //             $scope.totalItems = data.data.totalElements;
+    //             $scope.currentPage = data.data.number+1;
+    //         }else{
+    //             layui.layer.alert(data.msg);
+    //         }
+    //     })
+    //         .error(function (response,status,header) {
+    //             layui.layer.alert('系统繁忙、稍后再试');
+    //         });
+    // }
+    //
+    // $scope.getConsumeCountList();
+    //
+    // /**
+    //  * 查询充值金额信息
+    //  */
+    // $scope.getRechargeFeeList = function(){
+    //     $http({
+    //         method:"POST",
+    //         url:base_url+"/user/customer/findAllUser",
+    //         data:{
+    //             page:$scope.currentPage,
+    //             size:$scope.prePage,
+    //             shopId:admin_user.shopId
+    //         },
+    //         cache:false,
+    //     }).success(function (data,status) {
+    //         if(data.code=='0'){
+    //             console.log(data.data);
+    //             $scope.userList = data.data.content;
+    //             $scope.totalItems = data.data.totalElements;
+    //             $scope.currentPage = data.data.number+1;
+    //         }else{
+    //             layui.layer.alert(data.msg);
+    //         }
+    //     })
+    //         .error(function (response,status,header) {
+    //             layui.layer.alert('系统繁忙、稍后再试');
+    //         });
+    // }
+    //
+    // $scope.getRechargeFeeList();
 
     /**
      * 查询会员总数
      */
-    $scope.getUserTotal = function(){
-        $http({
-            method:"POST",
-            url:base_url+"/user/customer/findAllUser",
-            data:{
-                page:$scope.currentPage,
-                size:$scope.prePage,
-                shopId:admin_user.shopId
-            },
-            cache:false,
-        }).success(function (data,status) {
-            if(data.code=='0'){
-                console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
-            }else{
-                layui.layer.alert(data.msg);
-            }
-        })
-            .error(function (response,status,header) {
-                layui.layer.alert('系统繁忙、稍后再试');
-            });
-    }
-
-    $scope.getUserTotal();
-
-    /**
-     * 查询充值总金额
-     */
-    $scope.getRechargeFeeTotal = function(){
-        $http({
-            method:"POST",
-            url:base_url+"/user/customer/findAllUser",
-            data:{
-                page:$scope.currentPage,
-                size:$scope.prePage,
-                shopId:admin_user.shopId
-            },
-            cache:false,
-        }).success(function (data,status) {
-            if(data.code=='0'){
-                console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
-            }else{
-                layui.layer.alert(data.msg);
-            }
-        })
-            .error(function (response,status,header) {
-                layui.layer.alert('系统繁忙、稍后再试');
-            });
-    }
-
-    $scope.getRechargeFeeTotal();
-
-    /**
-     * 查询今日消费
-     */
-    $scope.getConsumeFeeToday = function(){
-        $http({
-            method:"POST",
-            url:base_url+"/user/customer/findAllUser",
-            data:{
-                page:$scope.currentPage,
-                size:$scope.prePage,
-                shopId:admin_user.shopId
-            },
-            cache:false,
-        }).success(function (data,status) {
-            if(data.code=='0'){
-                console.log(data.data);
-                $scope.userList = data.data.content;
-                $scope.totalItems = data.data.totalElements;
-                $scope.currentPage = data.data.number+1;
-            }else{
-                layui.layer.alert(data.msg);
-            }
-        })
-            .error(function (response,status,header) {
-                layui.layer.alert('系统繁忙、稍后再试');
-            });
-    }
-
-    $scope.getConsumeFeeToday();
+    // $scope.getUserTotal = function(){
+    //     $http({
+    //         method:"POST",
+    //         url:base_url+"/statistics/getUserTotal",
+    //         data:{
+    //             page:$scope.currentPage,
+    //             size:$scope.prePage,
+    //             shopId:admin_user.shopId
+    //         },
+    //         cache:false,
+    //     }).success(function (data,status) {
+    //         if(data.code=='0'){
+    //             console.log(data.data);
+    //             $scope.userList = data.data.content;
+    //             $scope.totalItems = data.data.totalElements;
+    //             $scope.currentPage = data.data.number+1;
+    //         }else{
+    //             layui.layer.alert(data.msg);
+    //         }
+    //     })
+    //         .error(function (response,status,header) {
+    //             layui.layer.alert('系统繁忙、稍后再试');
+    //         });
+    // }
+    //
+    // $scope.getUserTotal();
+    //
+    // /**
+    //  * 查询充值总金额
+    //  */
+    // $scope.getRechargeFeeTotal = function(){
+    //     $http({
+    //         method:"POST",
+    //         url:base_url+"/statistics/getRechargeFeeTotal",
+    //         data:{
+    //             page:$scope.currentPage,
+    //             size:$scope.prePage,
+    //             shopId:admin_user.shopId
+    //         },
+    //         cache:false,
+    //     }).success(function (data,status) {
+    //         if(data.code=='0'){
+    //             console.log(data.data);
+    //             $scope.userList = data.data.content;
+    //             $scope.totalItems = data.data.totalElements;
+    //             $scope.currentPage = data.data.number+1;
+    //         }else{
+    //             layui.layer.alert(data.msg);
+    //         }
+    //     })
+    //         .error(function (response,status,header) {
+    //             layui.layer.alert('系统繁忙、稍后再试');
+    //         });
+    // }
+    //
+    // $scope.getRechargeFeeTotal();
+    //
+    // /**
+    //  * 查询今日消费
+    //  */
+    // $scope.getConsumeFeeToday = function(){
+    //     $http({
+    //         method:"POST",
+    //         url:base_url+"/statistics/getConsumeFeeToday",
+    //         data:{
+    //             page:$scope.currentPage,
+    //             size:$scope.prePage,
+    //             shopId:admin_user.shopId
+    //         },
+    //         cache:false,
+    //     }).success(function (data,status) {
+    //         if(data.code=='0'){
+    //             console.log(data.data);
+    //             $scope.userList = data.data.content;
+    //             $scope.totalItems = data.data.totalElements;
+    //             $scope.currentPage = data.data.number+1;
+    //         }else{
+    //             layui.layer.alert(data.msg);
+    //         }
+    //     })
+    //         .error(function (response,status,header) {
+    //             layui.layer.alert('系统繁忙、稍后再试');
+    //         });
+    // }
+    //
+    // $scope.getConsumeFeeToday();
 
 }]);
